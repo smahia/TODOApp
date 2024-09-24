09c4c53fc806227b86b64d1481d2c71fa3b16dfc
Sara Mahía Gómez <saramahiagomez@gmail.com>
2024-09-24 15:26:34 +0200

    chore: change database from MySQL to SQLite

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 import eel
-import mysql.connector
+import sqlite3
 
 # Creating connection object
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="todo_app"
-)
+sqliteConnection = sqlite3.connect('todo_app.db')
 
 # Creating an instance of 'cursor' class
 # which is used to execute the 'SQL'
 # statements in 'Python'
-cursor = mydb.cursor()
+cursor = sqliteConnection.cursor()
 
-# Create table if not exists
-cursor.execute("CREATE TABLE IF NOT EXISTS task (id INT AUTO_INCREMENT PRIMARY KEY, task VARCHAR(255), status VARCHAR(255))")
+# Create table if not exists. In SQLite, INTEGER PRIMARY KEY column is auto-incremented
+cursor.execute("CREATE TABLE IF NOT EXISTS task (id INTEGER PRIMARY KEY, task VARCHAR(255), status VARCHAR(255))")
 
 eel.init('Gui')  # Specify the folder where your index.html file is located
 
@@ -34,28 +29,28 @@ def show_tasks():
 # Delete a task
 @eel.expose
 def delete_task(task_id):
-    query = "DELETE FROM task where id = %s"
+    query = "DELETE FROM task WHERE id = ?"
     cursor.execute(query, (task_id,))
-    mydb.commit()
+    sqliteConnection.commit()
 
 # Add a task
 @eel.expose
 def create_task(task, task_status):
-    query = "INSERT INTO task (task, status) VALUES (%s, %s)"
+    query = "INSERT INTO task (task, status) VALUES (?, ?)"
     cursor.execute(query, (task, task_status))
-    mydb.commit()
+    sqliteConnection.commit()
 
 # Edit a task
 @eel.expose
 def edit_task(task, task_status, task_id):
-    query = "UPDATE task SET task = %s, status = %s WHERE id = %s"
+    query = "UPDATE task SET task = ?, status = ? WHERE id = ?"
     cursor.execute(query, (task, task_status, task_id))
-    mydb.commit()
+    sqliteConnection.commit()
 
 # Get a task
 @eel.expose
 def get_task(task_id):
-    query = "SELECT * FROM task WHERE id = %s"
+    query = "SELECT * FROM task WHERE id = ?"
     cursor.execute(query, (task_id,))
     task = cursor.fetchone()
     return task
@@ -65,4 +60,4 @@ eel.start('index.html', size=(1000, 600))  # Run App window
 
 # Closing the connection
 cursor.close()
-mydb.close()
+sqliteConnection.close()
